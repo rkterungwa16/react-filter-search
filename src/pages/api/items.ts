bc35867071faaa70effa8ccabfe7ffb66a7b3d38
rkterungwa16 <terungwakombol@gmail.com>
2023-05-03 13:29:04 +0100

    implement filter api

---
+++ src/pages/api/items.ts
@@ -0,0 +1,33 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import queryString from "query-string";
+
+import { items } from "../../data";
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<
+    {
+      order: string;
+      type: string;
+      item: string;
+      category: string;
+    }[]
+  >
+) {
+  const { query, body } = req;
+  let filteredItems = items;
+  filteredItems = filteredItems.filter((_item: any) => {
+    const filterKeys = Object.keys(query);
+    const validItems = filterKeys.every((_key: string) => {
+      if (_item[_key]) {
+        return _item[_key] === query[_key];
+      }
+      return false;
+    })
+    return validItems;
+  });
+  const search = query?.search as string;
+  const searchItems = search?.split(",");
+  res.status(200).json(filteredItems);
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
+import queryString from 'query-string';
 import { SlideInContext } from "@/components/RightSlideIn/context";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { Layout } from "../components/Layout";
 
@@ -13,11 +14,27 @@ const Home = () => {
   ) => {
     setFilterValues(filterValues);
   };
-  console.log('context filter values -->>', filterValues);
+  useEffect(() => {
+    let query = '';
+    if (filterValues.length) {
+      query = filterValues?.map(
+        (_value) =>
+          `${_value.name}=${_value.value}`
+      ).join("&");
+    }
+    console.log('query -->>', query);
+    console.log('filter values -->>', filterValues);
+    (async () => {
+      const resultsResponse = await fetch(`http://localhost:3000/api/items${ query ? `?${query}` : ''}`);
+      const results = await resultsResponse.json();
+      console.log('request', results)
+    })();
+  }, [filterValues]);
+  console.log("context filter values -->>", filterValues);
   return (
     <SlideInContext.Provider value={{ handleFilterValues }}>
       <Layout>
-
+        child
       </Layout>
     </SlideInContext.Provider>
   );
